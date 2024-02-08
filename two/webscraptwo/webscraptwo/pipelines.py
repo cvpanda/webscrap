07a6d3be24cec70460b19bf8f24aad99f68a1343
cvpanda <nahuelcasagrande@gmail.com>
2024-02-08 10:55:54 -0300

    pipeline update

--- two/webscraptwo/webscraptwo/pipelines.py
+++ two/webscraptwo/webscraptwo/pipelines.py
@@ -77,7 +77,7 @@ class SaveToMySQLPipeline:
         self.conn = mysql.connector.connect(
             host = 'localhost',
             user = 'root',
-            password = '', #add your password here if you have one set 
+            password = '94RP8kzmp25!', #add your password here if you have one set 
             database = 'librarybooks'
         )
 
